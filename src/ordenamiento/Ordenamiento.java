f6828d893da209e30c4137e6068adb111905d25c
samuelmesa01-cloud <est.samuel.mesa@unimilitar.edu.co>
2026-04-07 10:02:23 -0500

    Se modificó el programa para permitir que el usuario ingrese la cantidad de datos y sus valores por teclado además, se integró el método de ordenamiento para mostrar el arreglo ordenado al final

--- src/ordenamiento/Ordenamiento.java
+++ src/ordenamiento/Ordenamiento.java
@@ -1,17 +1,34 @@
 package ordenamiento;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Ordenamiento {
+
     public static void main(String[] args) {
-        int arr[] = {5, 2, 11, -8, 115, 56, -3};
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿Cuántos números desea ingresar?: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Ingrese el valor #" + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+
         ordenar(arr);
-        System.out.println(java.util.Arrays.toString(arr));
+        System.out.println("Arreglo ordenado: " + Arrays.toString(arr));
     }
+
     private static void ordenar(int v[]) {
-        for (int i = 0; i < v.length-1; i++) {
+        for (int i = 0; i < v.length - 1; i++) {
             for (int j = i + 1; j < v.length; j++) {
                 if (v[i] > v[j]) {
-                    int w = v[j];
-                    v[j] = v[i];
-                    v[i] = w;
+                    int temp = v[i];
+                    v[i] = v[j];
+                    v[j] = temp;
                 }
             }
         }
